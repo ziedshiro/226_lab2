27d1cc1973a85c6a5e8df530cedb9fdaeeddd62c
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-11 18:00:37 +0700

    6320502991_2_4

--- 6320502991_2_4.c
+++ 6320502991_2_4.c
@@ -2,16 +2,17 @@
 int main()
 {
     char x;
-    int n;
+    int n,z;
     scanf("%c",&x);
     scanf("%d",&n);
+    z=n;
     if((x>='A'&&x<='Z')&&(n>=1&&n<=31))
     {
                    if(x>='A'&&x<='I')
     {
-        if(n%10==5)
+        if(z%10==5)
             printf("%c",92);
-            if(n%2==0)
+            if(z%2==0)
             {
             printf("{@_@}");
             }
@@ -19,15 +20,15 @@ int main()
             {
             printf("(^_^)");
             }
-        if(n%10==5)
+        if(z%10==5)
             printf("/");
 
     }
     else if(x>='J'&&x<='R')
     {
-        if(n%10==5)
+        if(z%10==5)
             printf("\\");
-            if(n%2==0)
+            if(z%2==0)
             {
             printf("{*v*}");
             }
@@ -35,14 +36,14 @@ int main()
             {
             printf("(*o*)");
             }
-        if(n%10==5)
+        if(z%10==5)
             printf("/");
     }
     else if(x>='S'&&x<='Z')
     {
-        if(n%10==5)
+        if(z%10==5)
             printf("\\");
-            if(n%2==0)
+            if(z%2==0)
             {
             printf("{x_x}");
             }
@@ -50,7 +51,7 @@ int main()
             {
             printf("(T_T)");
             }
-        if(n%10==5)
+        if(z%10==5)
             printf("/");
     }
     }
