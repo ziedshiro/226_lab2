a8ac4b015bc7a866edcee077cb72283ff9eb51aa
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-11 15:10:43 +0700

    6320502991_2_2

--- 6320502991_2_2.cpp
+++ 6320502991_2_2.cpp
@@ -4,16 +4,26 @@ int main()
 {
     char n;
     cin>>n;
-    int N;
+    int N,x;
     cin>>N;
     if(n=='A')
     {
         if(N<=200)
             cout<<199;
+        else
+        {
+            x-=200;
+            cout<<199+(x*3);
+        }
     }
     else if(n=='B')
     {
         if(N<=400)
             cout<<400;
+        else
+        {
+            x-=400;
+            cout<<199+(x*3);
+        }
     }
 }
