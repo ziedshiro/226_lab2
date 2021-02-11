dd6acf77d48efd9b59b494ff501c1a318c6dfc29
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-11 16:49:09 +0700

    6320502991_2_3

--- 6320502991_2_3.c
+++ 6320502991_2_3.c
@@ -1,42 +1,56 @@
 #include<stdio.h>
-#include<string.h>
 int main()
 {
-   int j,i,n[3],a;
+   int j,i,n[3],a,b,c,mx,mn,md;
    for(i=0;i<3;i++)
         scanf("%d",&n[i]);
    char x[3];
    scanf("%s",x);
-   for(j=0;j<3;j++)
-   {
-       for(i=0;i<3;i++)
+   mx=n[0];
+   for(i=0;i<3;i++)
+    if(n[i]>mx)
     {
-    a=n[i];
-    if(n[i]>n[i+1])
-        {
-            n[i]=n[i+1];
-            n[i+1]=a;
-        }
+        mx=n[i];
+        a=i;
     }
-   }
+    mn=n[0];
+    for(i=0;i<3;i++)
+        if(n[i]<mn)
+    {
+        mn=n[i];
+        b=i;
+    }
+      if(a==0&&b==1)
+            c=2;
+      else if(a==0&&b==2)
+        c=1;
+      else if(a==1&&b==0)
+        c=2;
+      else if(a==1&&b==2)
+        c=0;
+      else if(a==2&&b==0)
+        c=1;
+      else if(a==2&&b==1)
+        c=0;
+        md=n[c];
     if(x[0]=='A')
-        printf("%d ",n[0]);
+        printf("%d ",mn);
     else if(x[0]=='B')
-        printf("%d ",n[1]);
+        printf("%d ",md);
     else if(x[0]=='C')
-        printf("%d ",n[2]);
+        printf("%d ",mx);
     if(x[1]=='A')
-        printf("%d ",n[0]);
+        printf("%d ",mn);
     else if(x[1]=='B')
-        printf("%d ",n[1]);
+        printf("%d ",md);
     else if(x[1]=='C')
-        printf("%d ",n[2]);
+        printf("%d ",mx);
     if(x[2]=='A')
-        printf("%d",n[0]);
+        printf("%d",mn);
     else if(x[2]=='B')
-        printf("%d",n[1]);
+        printf("%d",md);
     else if(x[2]=='C')
-        printf("%d",n[2]);
+        printf("%d",mx);
 
 
 
