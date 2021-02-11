85f7651e05ed5d6e8317f5ba3e2dbe4108c6f809
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-11 16:04:59 +0700

    6320502991_2_3

--- 6320502991_2_3.cpp
+++ 6320502991_2_3.cpp
@@ -6,6 +6,7 @@ int main()
    for(i=0;i<3;i++)
         cin>>n[i];
    char x;
+   cin>>x;
    for(j=0;j<3;j++)
    for(i=0;i<3;i++)
     {
@@ -16,30 +17,9 @@ int main()
             n[i+1]=a;
         }
     }
-   if(x=="ABC")
-   {
+   if(x=='ABC')
     cout<<n[0]<<" "<<n[1]<<" "<<n[2];
-   }
-   else if(x=="ACB")
-    {
-    cout<<n[0]<<" "<<n[2]<<" "<<n[1];
-    }
-    else if(x=="BAC")
-    {
-    cout<<n[1]<<" "<<n[0]<<" "<<n[2];
-    }
-    else if(x=="BCA")
-    {
-    cout<<n[1]<<" "<<n[2]<<" "<<n[0];
-    }
-    else if(x=="CAB")
-    {
-    cout<<n[2]<<" "<<n[0]<<" "<<n[1];
-    }
-    else if(x=="CBA")
-    {
-    cout<<n[2]<<" "<<n[1]<<" "<<n[0];
-    }
+
 
 
 }
