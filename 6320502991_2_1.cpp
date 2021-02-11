b903d0006a12d5a85f84398c8fb0b433db36bfa1
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-11 14:47:43 +0700

    6320502991_2_1

--- 6320502991_2_1.cpp
+++ 6320502991_2_1.cpp
@@ -4,6 +4,35 @@ int main()
 {
     int a,b,c;
     cin>>a;
-    cin>>b;
-    cin>>c;
+    if(a>=0&&a<=30)
+    {
+       cin>>b;
+       if(b>=0&&b<=30)
+       {
+          cin>>c;
+          if(c>=0&&c<=40)
+          {
+              if(a+b+c>=80&&a+b+c<=100)
+                cout<<"A";
+              else if(a+b+c>=75&&a+b+c<=79)
+                cout<<"B+";
+              else if(a+b+c>=70&&a+b+c<=74)
+                cout<<"B";
+              else if(a+b+c>=65&&a+b+c<=69)
+                cout<<"C+";
+                else if(a+b+c>=60&&a+b+c<=64)
+                cout<<"C";
+                else if(a+b+c>=55&&a+b+c<=59)
+                cout<<"D+";
+                else if(a+b+c>=50&&a+b+c<=54)
+                cout<<"D";
+                else if(a+b+c>=0&&a+b+c<=49)
+                cout<<"F";
+          }
+       }
+
+    }
+
+
+
 }
