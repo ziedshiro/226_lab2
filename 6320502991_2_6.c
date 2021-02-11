547aea183c9326bbe1fa2a692102556431aba9de
ziedshiro <78530408+ziedshiro@users.noreply.github.com>
2021-02-11 17:27:48 +0700

    6320502991_2_6

--- 6320502991_2_6.c
+++ 6320502991_2_6.c
@@ -12,5 +12,29 @@ int main()
     }while(n>0);
     k=i;
     for(i=k-1;i>=0;i--)
-        printf("%d",N[i]);
+    {
+        if(N[i]==0)
+            printf("Zero");
+        else if(N[i]==1)
+            printf("One");
+        else if(N[i]==2)
+            printf("Two");
+            else if(N[i]==3)
+            printf("Three");
+            else if(N[i]==4)
+            printf("Four");
+            else if(N[i]==5)
+            printf("Five");
+            else if(N[i]==6)
+            printf("Six");
+            else if(N[i]==7)
+            printf("Seven");
+            else if(N[i]==8)
+            printf("Eight");
+            else if(N[i]==9)
+            printf("Nine");
+            if(i!=0)
+        printf("-");
+    }
+
 }
